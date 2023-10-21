f28b282bd3b6c55a48865a0e45ebf8c503a1a969
Narges1374 <narges.timachi@gmail.com>
2023-10-21 21:42:24 +0330

    add git address

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer>
       <a
-        href="https://github.com/Narges1374/Weather.git"
+        href="https://github.com/Narges1374/react-weather"
         target="_blank"
         rel="noreferrer"
       >
